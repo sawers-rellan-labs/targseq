7e06906ad5dfab833d5d3170d077d689d24d60b8
faustovrz <faustovrz@gmail.com>
2025-04-18 20:32:11 -0400

    Long form demo.R. Added quickstart.R

---
+++ scripts/quick_start.R
@@ -0,0 +1,56 @@
+# Required libraries
+library(targseq)
+library(Biostrings)
+library(phangorn)
+library(ape)
+library(ggtree)
+library(ggplot2)
+library(dplyr)
+
+# 1. Read data
+aln_file <- system.file("extdata", "hpc1_aligned.fasta", package="targseq")
+alignment <- readDNAStringSet(aln_file)
+taxa_info <- read.csv(system.file("extdata", "seqid_label.csv", package="targseq"))
+rownames(taxa_info) <- taxa_info$seqid
+taxa_info <- taxa_info %>% rename(ancestry_call = "founder_ancestry")
+
+# 2. Extract variant genotypes
+variants <- data.frame(
+  name = c("A204T", "I211V"),
+  pattern = c("GCCGTGGCGTGGCGC", "ATCACCCGC")
+)
+variant_data <- get_variant_gt(variants, alignment)
+
+# 3. Recode variants as REF/ALT
+variant_data$A204T <- c("ALT", "REF")[as.factor(variant_data$A204T)]
+variant_data$I211V <- c("REF", "ALT")[as.factor(variant_data$I211V)]
+rownames(variant_data) <- names(alignment)
+
+# 4. Build tree
+phyDat <- phyDat(read.dna(aln_file, format="fasta"), type="DNA")
+dna_dist <- dist.ml(phyDat, model="JC69")
+tree <- ladderize(upgma(dna_dist), right=FALSE)
+rotated_tree <- pivot_on(tree, "hpc1_B73")
+
+# 5. Create visualization
+tree_plot <- create_variant_heatmap_tree(
+  tree = rotated_tree, 
+  data = variant_data
+)
+
+# 6. Add ancestry information
+ancestry_palette <- c("Recurrent" = "tomato", "Donor" = "royalblue")
+enhanced_tree <- tree_plot %<+% taxa_info +
+  geom_tippoint(aes(color = ancestry_call), position = position_nudge(x = 0.0015)) +
+  scale_color_manual(values = ancestry_palette) +
+  theme(legend.position = c(0.25, 0.5))
+
+# 7. Add alignment view
+alignment_tree <- msaplot(enhanced_tree, 
+                          fasta = aln_file, 
+                          offset = 0.05, 
+                          width = 0.6)
+
+# 8. Save outputs
+ggsave(enhanced_tree, file = "maize_tree.pdf", height = 10, width = 7)
+ggsave(alignment_tree, file = "maize_tree_alignment.png", height = 12, width = 8)

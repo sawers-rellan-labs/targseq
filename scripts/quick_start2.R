93069c802ad70fe5a4018061bbfa56165c041094
faustovrz <faustovrz@gmail.com>
2025-04-22 21:20:13 -0400

    tree plot with leaftip dots, heatmaps and alignment implemented

--- scripts/quick_start2.R
+++ scripts/quick_start2.R
@@ -13,9 +13,9 @@ aln_file <- system.file("extdata", "hpc1_aligned.fasta", package="targseq")
 alignment <- readDNAStringSet(aln_file)
 alignment_order <- data.frame(seqid=names(alignment)) 
 
-# metadata_file <- system.file("extdata", "seqid_label.csv", package="targseq")
-metadata_file <- ('~/Desktop/seqid_label.csv')
-donor_file <- ('~/Desktop/donor_data.csv')
+metadata_file <- system.file("extdata", "seqid_label.csv", package="targseq")
+donor_file <- system.file("extdata", "donor_data.csv", package="targseq")
+
 donor_data <- read.csv(donor_file)
 
 # Read metadata 
@@ -28,6 +28,8 @@ taxa_info <- alignment_order  %>%
   left_join(donor_data, by=c(donor_accession= "donor_id")) %>%
   select(seqid, label=label_3,ancestry_call,elevation, nitrogen_0.5cm_mean_1000)
 
+names(alignment) <- taxa_info$label
+alignment
 
 # taxa_info <- read.csv(system.file("extdata", "seqid_label.csv", package="targseq"))
 rownames(taxa_info) <- NULL
@@ -62,32 +64,92 @@ taxa_info$label
 rotated_tree$tip.label
 hist(taxa_info$nitrogen_0.5cm_mean_1000)
 
-ggtree(rotated_tree,ladderize = FALSE) %<+% taxa_info[,-1] +
-  geom_tippoint(aes(color=nitrogen_0.5cm_mean_1000)) +
-  scale_color_continuous_divergingx(palette = 'ArmyRose', mid =300, min=100,n_interp = 25)  +
-  theme(legend.position = c(0.25, 0.5))
-
-ggtree(rotated_tree,ladderize = FALSE) %<+% taxa_info[,-1] +
-    geom_tippoint(aes(color=elevation)) +
-  scale_color_continuous_divergingx(palette = 'RdBu', mid =1500, n_interp = 25)  +
-  theme(legend.position = c(0.25, 0.5))
-                
-
-
 # 6. Add ancestry information
+
+ancestry <-  taxa_info[,c("label","ancestry_call"),drop=FALSE]
+rownames(ancestry) <- taxa_info$label
+
 ancestry_palette <- c("Recurrent" = "tomato", "Donor" = "royalblue")
-enhanced_tree <- tree_plot %<+% taxa_info[,-1] +
+
+p1 <- ggtree(rotated_tree , ladderize = FALSE,) %<+% ancestry +
   geom_tippoint(aes(color = ancestry_call), position = position_nudge(x = 0.0015)) +
-   scale_color_manual(values = ancestry_palette) +
+  geom_tiplab(size = 2.5, offset = 0.002, align = TRUE,linesize = 0.1) +
+  ggtree::vexpand(.1, 1) +
+  scale_color_manual(name="Ancestry call",values = ancestry_palette) +
+  theme(legend.position = c(0.25, 0.5)) +
+  theme_tree2()
+
+
+
+env_par <- taxa_info[,c("elevation","nitrogen_0.5cm_mean_1000"),drop= FALSE]
+colnames(env_par ) <- c("Elevation","Nitrogen")
+rownames(env_par ) <- taxa_info$label
+
+
+library(ggnewscale)
+
+p2 <-gheatmap( p1, 
+               data = env_par[,"Elevation",drop=FALSE], 
+              offset = 0.025, width = 0.15, 
+              colnames = TRUE, colnames_angle = 45, 
+              colnames_offset_y = 0.4, hjust = 0,
+              colnames_position = "top") +
+ scale_fill_continuous_divergingx(name = "Elevation (masl)", palette = 'RdBu', mid =1500, n_interp = 25) +
+#  guides(fill=guide_colourbar(title="Elevation\n(masl)")) +
   theme(legend.position = c(0.25, 0.5))
+
+
+
+
+# Define color palette for REF/ALT states
+ref_alt_colors <- c("REF" = "tomato", "ALT" = "royalblue")
+
+# Add heatmap alongside the tree
+# Each column represents a different variant or ancestry information
+p3 <- gheatmap(p2 + new_scale_fill(), variant_data , offset = 0.045, width = 0.12, 
+              colnames = TRUE, colnames_angle = 45, 
+              colnames_offset_y = 0.65, hjust = 0,
+              colnames_position = "top") +
+  scale_fill_manual(values = ref_alt_colors, name = "Allele") +
+  theme(legend.position = c(0.25, 0.5))
+
+
+
+
+
+
+# Define trimming ranges (starting from position 2226 to the end)
+
+library(GenomicRanges)
+library(Biostrings)
+n_taxa <- length(alignment) 
+n_pos <- width(alignment)[1]
+length(names(alignment))
+
+trimmedRanges <- GRanges(
+  seqnames = names(alignment),
+  ranges = IRanges(
+    start = rep(2226,n_taxa),
+    end = rep(n_pos, n_taxa)
+  )
+)
+
+# Perform the trimming operation
+trimmed_alignment <- alignment[trimmedRanges]
+trimmed_alignment <-  trimmed_alignment[rotated_tree$tip.label,]
+bin_alignment <- as.DNAbin(trimmed_alignment)
+all(names(bin_alignment) == rotated_tree$tip.label)
 
 
 # 7. Add alignment view
-alignment_tree <- msaplot(enhanced_tree, 
-                          fasta = aln_file, 
-                          offset = 0.05, 
-                          width = 0.6)
+
+alignment_tree <- msaplot(p3+new_scale_fill(), 
+                          fasta = bin_alignment , 
+                          offset = 0.06, 
+                          width = 0.5) +
+  guides(fill="none")+
+  theme(legend.position = c(0.25, 0.5))
 
 # 8. Save outputs
-ggsave(enhanced_tree, file = "maize_tree.pdf", height = 10, width = 7)
 ggsave(alignment_tree, file = "maize_tree_alignment.png", height = 12, width = 8)
+
